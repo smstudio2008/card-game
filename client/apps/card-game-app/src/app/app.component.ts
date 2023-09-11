19b22e867a3dc521a689fe7b8d3976b0c5b3d00a
Seid Mohamed, salem <sseidmohamed@deloitte.co.uk>
2023-09-11 22:57:26 +0100

    intial commit

---
+++ client/apps/card-game-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card-game-temp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'card-game-app';
+}
